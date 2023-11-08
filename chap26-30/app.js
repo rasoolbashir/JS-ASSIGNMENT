9e30dd87d2a259e2ccd191cf0b0ec9a1ca34788a
rasool bashir <bashirrasool0@gmail.com>
2023-11-07 23:53:57 -0800

    new work

---
+++ chap26-30/app.js
@@ -0,0 +1,62 @@
+// // Question No 1
+// //  let positiveInteger = Number(prompt("Enter an integer number"));
+// // Or we can simply declare a variable instead of prompt
+// /*
+//  let positiveInteger = 3.45214
+// let conIntoRound = Math.round(positiveInteger);
+// let conIntoRound2 = Math.floor(positiveInteger);
+// let conIntoRound3 = Math.ceil(positiveInteger);
+// document.write(Number: ${positiveInteger} <br>round off value: ${conIntoRound} <br>floor value: ${conIntoRound2} <br>Ceil value: ${conIntoRound3});
+
+// // Question No 2
+// let negativePoint = -2.673
+// let negIntoRound = Math.round(negativePoint);
+// let negIntofloor = Math.floor(negativePoint);
+// let negIntoCeil = Math.ceil(negativePoint);
+// document.write(<br><br>Number: ${negativePoint} <br>round off value: ${negIntoRound} <br>floor value: ${negIntofloor} <br>Ceil value: ${negIntoCeil});
+
+// // Question No 3
+// let absNumber = -4;
+// let absNumber2 = Math.abs(absNumber);
+// document.write(<br><br>The absolute value of ${absNumber} is ${absNumber2});
+
+// // Question No 4
+// let randomDice = Math.round(Math.random()* 10)
+// console.log(random dice value: ${randomDice});
+
+// // Question No 5
+// let randomCoinValue = ["Tails","Heads"];
+// let emptyStr = "";
+// var randomNum;
+// for (var i = 0; i < randomCoinValue.length; i++) {
+//      randomNum = Math.floor(Math.random()* randomCoinValue.length);
+//     emptyStr = randomCoinValue[randomNum]
+// }
+// document.write(<br><br>${randomNum} random coin value: ${emptyStr});
+
+// // Question No 6
+
+// //let randomNumShow = Math.round(Math.random() * 100);
+// //console.log(Random number between 1 and 100: ${randomNumShow});
+
+// //Question No 7
+// let askUserWeight =prompt("Enter your weight in kilograms");
+//  switch (askUserWeight) {
+//     case "Kgs":
+//         console.log(askUserWeight);
+//         break;
+ 
+//     default:
+//         console.log();
+//         break;
+//  }
+// */
+//  // Question No 8
+// // let secNum =Number(prompt("Enter a number between 1 and 10"));
+// // let numberGuess = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// // let randomNum = Math.floor(Math.random() * numberGuess.length) + 1;
+// // if (secNum === numberGuess[5]) {
+// //     alert(Your secret number is ${secNum})
+// // }else{
+// //     alert("Try again!");
+// // }
